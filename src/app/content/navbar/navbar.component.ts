fe5edf2dfa9e069809517990f15a7b14f0459cef
Thomas Brunet <dev@thomas-brunet.fr>
2019-06-13 14:58:03 +0200

    add btn and context menu notification

--- src/app/content/navbar/navbar.component.ts
+++ src/app/content/navbar/navbar.component.ts
@@ -30,7 +30,20 @@ export class NavbarComponent implements OnInit {
         }
     ];
 
-    ngOnInit() {}
+    // to be managaed on back
+    notifications = [
+        {
+            title: 'Im the notif 1',
+            link: '/content/notifications/1'
+        },
+        {
+            title: 'Im the notif 2',
+            link: '/content/notifications/2'
+        }
+    ];
+
+    ngOnInit() {
+    }
 
     toggle() {
         this.sidebarService.toggle(true);
